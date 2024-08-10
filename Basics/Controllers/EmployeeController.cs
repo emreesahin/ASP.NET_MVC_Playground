a54e23f1da069a26c8561233ff4276738b8a7357
Emre Şahin <190254064@ogr.alanya.edu.tr>
2024-08-10 18:47:11 +0300

    Employee Controller Pages Added

---
+++ Basics/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+using System.Runtime.CompilerServices;
+using Basic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index1() {
+            string message = $"Hello World. {DateTime.Now.ToString()}";
+            return View("Index1", message); 
+        }
+
+        public ViewResult Index2 () {
+            var names = new String[]{
+                "Ahmet",
+                "Mehmet", 
+                "Ali"
+            };
+            return View(names);        
+            }
+
+        public IActionResult Index3() {
+            var list = new List<Employee>() {
+                new Employee () {Id= 1, FirstName = "Ahmet", LastName = "Can", Age = 20 },
+                new Employee () {Id= 2, FirstName = "Can", LastName = "Dag", Age = 22 },
+                new Employee () {Id= 3, FirstName = "Emre", LastName = "Gunes", Age = 37 }
+            };
+            return View("Index3" , list);
+        }
+    }
+}
--- Basics/obj/Debug/net6.0/Basics.AssemblyInfo.cs
+++ Basics/obj/Debug/net6.0/Basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f89d6cdbdb2c6a3b961da6e506e0ca898556b6e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
