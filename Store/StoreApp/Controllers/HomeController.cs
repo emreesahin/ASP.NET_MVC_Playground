2b797189f2c3a2674dad4a8af7ac4adbceb47cf0
Emre Şahin <190254064@ogr.alanya.edu.tr>
2024-08-16 17:05:19 +0300

    UI Updates Integrated

--- Store/StoreApp/Controllers/HomeController.cs
+++ Store/StoreApp/Controllers/HomeController.cs
@@ -4,8 +4,9 @@ namespace StoreApp.Controllers
 {
     public class HomeController : Controller
     {
-        public String Index() {
-            return "Hello Store App";
+        public IActionResult Index()
+        {
+            return View();
         }
     }
 }
